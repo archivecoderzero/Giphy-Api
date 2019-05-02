2a76ceb45062c42ecfebd2186a6cb5e480cff3ff
unknown <archivecoderzero@gmail.com>
2019-05-01 20:39:46 -0700

    updated css and html

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,33 @@
+    var apiKey = "7MTC1g2AgJ1zKwZ1HoRkVWLwVHIP9Rqp"
+
+
+    $("#cat-button").on("click", function() {
+      var request = prompt("What Gif do you Want?")
+
+      //
+
+      var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=" + apiKey +  "&tag=" + request ;
+
+      //
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+
+      //
+        .then(function(response) {
+
+        //
+          var imageUrl = response.data.image_original_url;
+
+          //
+          var catImage = $("<img>");
+
+          //
+          catImage.attr("src", imageUrl);
+          catImage.attr("alt", "cat image");
+
+          //
+          $("#images").prepend(catImage);
+        });
+    });
